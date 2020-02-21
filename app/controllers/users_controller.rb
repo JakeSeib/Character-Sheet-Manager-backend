bf4c1282ec152a606b1d76b233b5496dec4e7568
JakeSeib <jacob.a.seib@gmail.com>
2020-02-21 12:53:24 -0500

    Fix user's name attribute
    Add name as a permitted attribute in user controller
    Show name in user serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ProtectedController
 
   def user_creds
     params.require(:credentials)
-          .permit(:email, :password, :password_confirmation)
+          .permit(:email, :name, :password, :password_confirmation)
   end
 
   def pw_creds
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email
+  attributes :id, :email, :name
   has_many :characters
 end
